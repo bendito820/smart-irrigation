136cd8cd43484efa51d11756573f134033733765
Antonio Costa <bendito820@gmail.com>
2024-04-26 14:08:56 +0100

    Create AppText component

---
+++ app/components/AppText.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode } from "react";
+import { StyleProp, StyleSheet, Text, ViewStyle } from "react-native";
+
+import { Platform } from "react-native";
+
+interface Props {
+  children: ReactNode;
+  style?: StyleProp<ViewStyle>;
+}
+
+function AppText({ children, style, ...otherProps }: Props) {
+  return (
+    <Text style={[styles.text, style]} {...otherProps}>
+      {children}
+    </Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: "#fff",
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+  },
+});
+
+export default AppText;
--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import Text from "../components/AppText";
 
 import Screen from "../components/Screen";
 
 const HomeScreen = () => {
   return (
     <Screen>
-      <Text style={{ color: "#fff" }}>HomeScreen</Text>
+      <Text>HomeScreen</Text>
     </Screen>
   );
 };
