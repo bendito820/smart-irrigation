39ea3827227d8d2b6f5dae04db1fd47413047cc2
Antonio Costa <bendito820@gmail.com>
2024-04-27 08:33:29 +0100

    Create The toggle Component (Cross platform Ios Like Button)

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Text from "../components/AppText";
 import Screen from "../components/Screen";
 import { View } from "react-native";
 import Card from "../components/Card";
+import Toggle from "../components/Toggle";
 
 const HomeScreen = () => {
+  const [toggleIsOn, setToggle] = useState(false);
+
   return (
     <Screen style={{ paddingHorizontal: 8 }}>
       <Text>HomeScreen</Text>
       <Card label="Temperatura" value={30} icon="°C" />
+      <Toggle
+        isOn={toggleIsOn}
+        onToggle={() => {
+          setToggle(!toggleIsOn);
+        }}
+      />
+      {toggleIsOn && <Text>Button is on</Text>}
     </Screen>
   );
 };
