e8d885a38034777eefb96721faf2306bdb1d783f
Antonio Costa <bendito820@gmail.com>
2024-04-28 10:11:09 +0100

    Implement Auto Mode based on Tuggle State

--- app/components/AutoMode.tsx
+++ app/components/AutoMode.tsx
@@ -1,11 +1,36 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import axios from "axios";
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Toggle from "./Toggle";
+import { StyleSheet, View } from "react-native";
 import AppText from "./AppText";
+import Toggle from "./Toggle";
 
 export default function AutoMode() {
   const [toggleIsOn, setToggle] = useState<boolean>(false);
+
+  const handleSubmit = async () => {
+    setToggle(!toggleIsOn);
+
+    const requestData = toggleIsOn
+      ? {
+          mode: "manual",
+          pump: "off", // Toggle pump off based on toggle state
+        }
+      : {
+          mode: "auto", // Toggle auto mode on based on toggle state
+        };
+
+    try {
+      // http://esp8266-control/pump-control
+      await axios.post("http://192.168.0.28/pump-control", requestData, {
+        headers: {
+          "Content-Type": "application/json", // Specify content type as JSON
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -27,7 +52,7 @@ export default function AutoMode() {
       >
         Modo automático
       </AppText>
-      <Toggle isOn={toggleIsOn} onToggle={() => setToggle(!toggleIsOn)} />
+      <Toggle isOn={toggleIsOn} onToggle={handleSubmit} />
     </View>
   );
 }
